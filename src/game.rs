d048021699f593e26c336f9cf3aeeff787e25159
Nikita Medvedev <some.redteapot@gmail.com>
2022-12-13 01:40:29 +0700

    Update to Bevy 0.9.1, major refactor

--- src/game.rs
+++ src/game.rs
@@ -1,12 +1,39 @@
+// Don't touch this piece, needed for Web
+#[cfg(target_arch = "wasm32")]
+mod web_main;
+
 use bevy::prelude::*;
-use bevy_kira_audio::AudioPlugin;
+use bevy_kira_audio::{Audio, AudioControl, AudioPlugin};
 
 pub fn run(app: &mut App) {
-    app.insert_resource(WindowDescriptor {
-        title: "KPACUBO".to_string(),
-        ..Default::default()
-    })
-    .add_plugins(DefaultPlugins)
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        window: WindowDescriptor {
+            title: "Game".to_string(),
+            ..default()
+        },
+        ..default()
+    }))
     .add_plugin(AudioPlugin)
+    .add_startup_system(setup)
     .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    assets: Res<AssetServer>,
+    audio: Res<Audio>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn(SpriteBundle {
+        texture: assets.load("character.png"),
+        sprite: Sprite {
+            color: Color::WHITE,
+            ..default()
+        },
+        ..default()
+    });
+
+    audio.play(assets.load("powerup.wav"))
+        .with_volume(0.7);
 }
--- src/lib.rs
+++
@@ -1,6 +0,0 @@
-#[cfg(target_arch = "wasm32")]
-mod web_main;
-
-mod game;
-
-pub use game::*;
